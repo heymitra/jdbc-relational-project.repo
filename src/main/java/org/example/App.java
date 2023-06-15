7f64bb49bd2386f4e00f25a6fa484559d2e13830
mitrahe <heydarimitra98@gmail.com>
2023-06-15 02:28:14 +0000

    Add App class (where program starts).

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.example.ui.Menu;
+
+import java.sql.SQLException;
+
+public class App {
+    public static void main(String[] args) throws SQLException {
+
+        while (!Menu.stop) {
+            Menu.run();
+        }
+
+    }
+}
